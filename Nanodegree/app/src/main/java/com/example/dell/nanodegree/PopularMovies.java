98a422cf4ba70503f76c82e23ec03bce8e83668f
oasis9594 <oasis9594@gmail.com>
2016-07-07 10:31:45 +0530

    Bug Fixes

--- Nanodegree/app/src/main/java/com/example/dell/nanodegree/PopularMovies.java
+++ Nanodegree/app/src/main/java/com/example/dell/nanodegree/PopularMovies.java
@@ -25,76 +25,73 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
 public class PopularMovies extends AppCompatActivity {
 
     RecyclerView popularMovieList;
-    public static final String TAG="dell.nanodegree.TAG";
-    ArrayList<MovieObject> movieObjects;
+    public static final String TAG = "dell.nanodegree.TAG";
+    ArrayList<MovieObject> movieObjects = new ArrayList<>();
     PopularMovieAdapter movieAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_popular_movies);
-        assert getSupportActionBar()!=null;
+        assert getSupportActionBar() != null;
         getSupportActionBar().setTitle("Popular Movies");
-        movieObjects=new ArrayList<>();
-        popularMovieList=(RecyclerView) findViewById(R.id.popularMovieList);
-        GridLayoutManager layoutManager=new GridLayoutManager(this, 2);
+        popularMovieList = (RecyclerView) findViewById(R.id.popularMovieList);
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
         popularMovieList.setLayoutManager(layoutManager);
-        SharedPreferences shared=PreferenceManager.getDefaultSharedPreferences(this);
-        String type=shared.getString(getResources().getString(R.string.movie_type), null);
-        if(type==null)
-            type="popular";
+        SharedPreferences shared = PreferenceManager.getDefaultSharedPreferences(this);
+        String type = shared.getString(getResources().getString(R.string.movie_type), null);
+        if (type == null)
+            type = "popular";
         getData(type);
-        movieAdapter=new PopularMovieAdapter(this, movieObjects);
+        movieAdapter = new PopularMovieAdapter(this, movieObjects);
         popularMovieList.setAdapter(movieAdapter);
     }
-    public void getData(String s)
-    {
+
+    public void getData(String s) {
         ConnectivityManager connMgr = (ConnectivityManager)
                 getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
         if (networkInfo != null && networkInfo.isConnected()) {
-            FetchMovieTask movieTask=new FetchMovieTask();
+            FetchMovieTask movieTask = new FetchMovieTask();
             setActionBarTitle(s);
             movieTask.execute(s);
         } else {
             Toast.makeText(getApplicationContext(), "No Internet Connection!!!!", Toast.LENGTH_SHORT).show();
         }
     }
-    public class FetchMovieTask extends AsyncTask<String, Void, ArrayList<MovieObject>>
-    {
+
+    public class FetchMovieTask extends AsyncTask<String, Void, ArrayList<MovieObject>> {
 
         @Override
-        protected void onPostExecute(ArrayList<MovieObject> m) {
-            if(m!=null)
-            {
+        protected void onPostExecute(ArrayList<MovieObject> myMovieList) {
+            if (myMovieList != null) {
                 movieObjects.clear();
-                for(MovieObject o: m)
-                {
+                for (MovieObject o : myMovieList) {
                     movieObjects.add(o);
                     Log.v(TAG, o.getOriginal_title());
                 }
-                Log.v(TAG, ""+movieObjects.size());
+                Log.v(TAG, "" + movieObjects.size());
                 movieAdapter.notifyDataSetChanged();
             }
         }
 
         @Override
         protected ArrayList<MovieObject> doInBackground(String... params) {
-            if(params.length==0)
+            if (params.length == 0)
                 return null;
-            HttpURLConnection urlConnection=null;
-            BufferedReader reader=null;
-            String movieJsonString=null;
-            final String MOVIE_API_KEY=getResources().getString(R.string.movie_key);
-            try{
-                final String BASE_API_POPULAR="http://api.themoviedb.org/3/movie";
-                final String QUERY_API_KEY="api_key";
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            String movieJsonString = null;
+            final String MOVIE_API_KEY = getResources().getString(R.string.movie_key);
+            try {
+                final String BASE_API_POPULAR = "http://api.themoviedb.org/3/movie";
+                final String QUERY_API_KEY = "api_key";
                 Uri builtUri = Uri.parse(BASE_API_POPULAR).buildUpon()
                         .appendPath(params[0])
                         .appendQueryParameter(QUERY_API_KEY, MOVIE_API_KEY).build();
@@ -129,12 +126,10 @@ public class PopularMovies extends AppCompatActivity {
                 }
                 movieJsonString = buffer.toString();
                 Log.v(TAG, movieJsonString);
-            }catch (IOException e)
-            {
-                Log.e(TAG, "Error getting data from api: "+e.getMessage());
+            } catch (IOException e) {
+                Log.e(TAG, "Error getting data from api: " + e.getMessage());
                 return null;
-            }
-            finally {
+            } finally {
                 if (urlConnection != null) {
                     urlConnection.disconnect();
                 }
@@ -148,17 +143,15 @@ public class PopularMovies extends AppCompatActivity {
             }
             try {
                 return getMovieDataFromJson(movieJsonString);
-            }catch (JSONException e)
-            {
-                Log.e(TAG, "Error parsing JSON: "+e.getMessage());
+            } catch (JSONException e) {
+                Log.e(TAG, "Error parsing JSON: " + e.getMessage());
             }
             return null;
         }
     }
-    public void setActionBarTitle(String s)
-    {
-        switch (s)
-        {
+
+    public void setActionBarTitle(String s) {
+        switch (s) {
             case "popular":
                 getSupportActionBar().setTitle("Popular Movies");
                 break;
@@ -170,42 +163,41 @@ public class PopularMovies extends AppCompatActivity {
                 break;
         }
     }
-    ArrayList<MovieObject> getMovieDataFromJson(String movieJsonStr) throws JSONException
-    {
-        ArrayList<MovieObject> myMovies=new ArrayList<>();
 
-        final String POSTER_BASE_URL="http://image.tmdb.org/t/p/w342";
-        final String MOVIE_RESULTS="results";
-        final String MOVIE_POSTER="poster_path";
-        final String MOVIE_ORIGINAL_TITLE="original_title";
-        final String MOVIE_OVERVIEW="overview";
-        final String MOVIE_LANGUAGE="original_language";
-        final String MOVIE_RATING="vote_average";
-        final String MOVIE_POPULARITY="popularity";
-        final String MOVIE_VOTE_COUNT="vote_count";
-        final String MOVIE_ADULT="adult";
-        final String MOVIE_DATE="release_date";
+    ArrayList<MovieObject> getMovieDataFromJson(String movieJsonStr) throws JSONException {
+        ArrayList<MovieObject> myMovies = new ArrayList<>();
+
+        final String POSTER_BASE_URL = "http://image.tmdb.org/t/p/w342";
+        final String MOVIE_RESULTS = "results";
+        final String MOVIE_POSTER = "poster_path";
+        final String MOVIE_ORIGINAL_TITLE = "original_title";
+        final String MOVIE_OVERVIEW = "overview";
+        final String MOVIE_LANGUAGE = "original_language";
+        final String MOVIE_RATING = "vote_average";
+        final String MOVIE_POPULARITY = "popularity";
+        final String MOVIE_VOTE_COUNT = "vote_count";
+        final String MOVIE_ADULT = "adult";
+        final String MOVIE_DATE = "release_date";
 
 
         JSONObject movieJson = new JSONObject(movieJsonStr);
-        JSONArray movieArray=movieJson.getJSONArray(MOVIE_RESULTS);
+        JSONArray movieArray = movieJson.getJSONArray(MOVIE_RESULTS);
         int i;
-        for(i=0;i<movieArray.length();i++)
-        {
-            MovieObject obj=new MovieObject();
+        for (i = 0; i < movieArray.length(); i++) {
+            MovieObject obj = new MovieObject();
             String posterPath;
             JSONObject movie = movieArray.getJSONObject(i);
 
-            posterPath = POSTER_BASE_URL+movie.getString(MOVIE_POSTER);
+            posterPath = POSTER_BASE_URL + movie.getString(MOVIE_POSTER);
 
-            String title=movie.getString(MOVIE_ORIGINAL_TITLE);
-            String overview=movie.getString(MOVIE_OVERVIEW);
-            String language=movie.getString(MOVIE_LANGUAGE);
-            double rating=movie.getDouble(MOVIE_RATING);
-            double popularity=movie.getDouble(MOVIE_POPULARITY);
-            int votes=movie.getInt(MOVIE_VOTE_COUNT);
-            boolean b=movie.getBoolean(MOVIE_ADULT);
-            String d=movie.getString(MOVIE_DATE);
+            String title = movie.getString(MOVIE_ORIGINAL_TITLE);
+            String overview = movie.getString(MOVIE_OVERVIEW);
+            String language = movie.getString(MOVIE_LANGUAGE);
+            double rating = movie.getDouble(MOVIE_RATING);
+            double popularity = movie.getDouble(MOVIE_POPULARITY);
+            int votes = movie.getInt(MOVIE_VOTE_COUNT);
+            boolean b = movie.getBoolean(MOVIE_ADULT);
+            String d = movie.getString(MOVIE_DATE);
 
             obj.setPosterPath(posterPath);
             Log.v(TAG, posterPath);
@@ -221,35 +213,32 @@ public class PopularMovies extends AppCompatActivity {
         }
         return myMovies;
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_movie_type, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id=item.getItemId();
-        if(id == R.id.menu_popular) {
-            SharedPreferences shared=PreferenceManager.getDefaultSharedPreferences(this);
-            SharedPreferences.Editor editor=shared.edit();
-            editor.clear();
+        int id = item.getItemId();
+        if (id == R.id.menu_popular) {
+            SharedPreferences shared = PreferenceManager.getDefaultSharedPreferences(this);
+            SharedPreferences.Editor editor = shared.edit();
             editor.putString(getResources().getString(R.string.movie_type), "popular");
             editor.apply();
             getData("popular");
-        }
-        else if(id == R.id.menu_top_rated) {
-            SharedPreferences shared=PreferenceManager.getDefaultSharedPreferences(this);
-            SharedPreferences.Editor editor=shared.edit();
-            editor.clear();
+        } else if (id == R.id.menu_top_rated) {
+            SharedPreferences shared = PreferenceManager.getDefaultSharedPreferences(this);
+            SharedPreferences.Editor editor = shared.edit();
             editor.putString(getResources().getString(R.string.movie_type), "top_rated");
             editor.apply();
             getData("top_rated");
-        }
-        else if(id == R.id.menu_upcoming) {
-            SharedPreferences shared=PreferenceManager.getDefaultSharedPreferences(this);
-            SharedPreferences.Editor editor=shared.edit();
-            editor.clear();
+        } else if (id == R.id.menu_upcoming) {
+            SharedPreferences shared = PreferenceManager.getDefaultSharedPreferences(this);
+            SharedPreferences.Editor editor = shared.edit();
             editor.putString(getResources().getString(R.string.movie_type), "upcoming");
             editor.apply();
             getData("upcoming");
